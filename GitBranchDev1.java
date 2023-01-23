510f7d54d5ec8193acb44a80f3a2b28cdef68559
Dondapati Yashaswi <dondapatiyashaswi@L-HH226K6WM3.local>
2023-01-23 11:55:08 +0530

    1st commit in dev1 branch

---
+++ GitBranchDev1.java
@@ -0,0 +1,11 @@
+package com.finserve.javatraining5;
+
+public class GitBranchDev1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("in the dev1 branch");
+
+	}
+
+}
