6f1d884bbc350ce308e7a95adb893c35aa6c6b41
Dondapati Yashaswi <dondapatiyashaswi@L-HH226K6WM3.local>
2023-01-23 15:18:27 +0530

    2nd file in 2nd branch

---
+++ GitBranchDev2.java
@@ -0,0 +1,11 @@
+package com.finserve.javatraining5;
+
+public class GitBranchDev2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("in the dev2 branch");
+
+	}
+
+}
